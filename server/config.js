23e9a41a95f46fb57b7fcd60e716844f4a07b10e
ruganga <ulanruganga@gmail.com>
2020-10-07 13:49:56 +0600

    set up boilerplate add lint, prettier and redux

--- server/config.js
+++
@@ -1,8 +0,0 @@
-const dotEnv = require('dotenv')
-dotEnv.config()
-
-const options = {
-  mongoUrl: process.env.MONGOURL
-}
-
-module.exports = options
---
+++ src/components/main.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { useSelector } from 'react-redux'
+
+const Main = () => {
+
+const user = useSelector((s) => s.sample.name)
+
+  return(
+    <div>
+      {user}
+    </div>
+  )
+}
+
+React.memo(Main)
+
+export default Main
